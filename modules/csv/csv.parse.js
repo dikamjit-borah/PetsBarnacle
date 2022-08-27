5cc39b518c2c1b8fe3ce1e3c451ddf659a4c86b0
dikamjit_borah <dikamjit.borah@superprocure.com>
2022-08-27 19:42:35 +0530

    parse pets_data.csv

---
+++ modules/csv/csv.parse.js
@@ -0,0 +1,18 @@
+const csv = require('csv-parser')
+const fs = require('fs')
+const path = require('path');
+const csvPath = path.join(__dirname, 'pet_data.csv');
+const results = [];
+
+module.exports = {
+    parseCsv: () => {
+        console.log(csvPath);
+        fs.createReadStream(csvPath)
+            .pipe(csv())
+            .on('data', (data) => results.push(data))
+            .on('end', () => {
+                console.log(results);
+            });
+    }
+}
+
--- server.js
+++ server.js
@@ -2,15 +2,18 @@ require('dotenv').config()
 
 const TAG = "server.js"
 
-const express = require('express')
+const csvParser = require('csv-parser');
+const express = require('express');
+const csvParse = require('./modules/csv/csv.parse');
 const app = express()
 const port = process.env.PORT || 5052;
 app.use(express.json())
 
-const mongoDbConnection = require('./modules/mongodb/mongodb.connect')
+//const mongoDbConnection = require('./modules/mongodb/mongodb.connect')
 
 app.listen(port, () => {
     console.log(TAG, `PetBarnacle running on port ${port}`)
+    csvParse.parseCsv()
 });
 
 
